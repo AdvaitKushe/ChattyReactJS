1f94561ed0f6ab1446b73ae9887788dab7f02c56
Advait Kushe <advait.kushe@gmail.com>
2024-02-19 18:49:50 -0500

    hotjaring

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 import { Box, ChakraProvider } from "@chakra-ui/react";
+import HotjarTracking from './HotjarTracking';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
+    <HotjarTracking />
       <App />
     </ChakraProvider>
   </React.StrictMode>
